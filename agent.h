6efb27ee26cf64ebe250666f3c27560d8f2ec08d
imsohandsome123 <imsohandsome123@gmail.com>
2023-01-10 05:59:38 +0800

    add rave algorithm

--- agent.h
+++ agent.h
@@ -120,7 +120,7 @@ public:
 		return random_player::take_action(state);
 	}
 
-	class node : board {
+	class node : board { //
 	public:
 		node(const board& state, node* parent = nullptr) : board(state),
 			win(0), visit(0), child(), parent(parent) {}
@@ -129,11 +129,13 @@ public:
 		 * run MCTS for N cycles and retrieve the best action
 		 */
 		action run_mcts(size_t N, std::default_random_engine& engine) {
+			std::unordered_map<board::piece_type, std::unordered_map<int, int>> rave_win;
+			std::unordered_map<board::piece_type, std::unordered_map<int, int>> rave_visit;
 			for (size_t i = 0; i < N; i++) {
-				std::vector<node*> path = select();
-				node* leaf = path.back()->expand(engine);
+				std::vector<node*> path = select(rave_win, rave_visit);
+				node* leaf = path.back()->expand(engine);.
 				if (leaf != path.back()) path.push_back(leaf);
-				update(path, leaf->simulate(engine));
+				update(path, leaf->simulate(engine), rave_win, rave_visit);
 			}
 			return take_action();
 		}
@@ -144,11 +146,15 @@ public:
 		 * select from the current node to a leaf node by UCB and return all of them
 		 * a leaf node can be either a node that is not fully expanded or a terminal node
 		 */
-		std::vector<node*> select() {
+		std::vector<node*> select(
+			std::unordered_map<board::piece_type, std::unordered_map<int, int>> rave_win,
+			std::unordered_map<board::piece_type, std::unordered_map<int, int>> rave_visit) {
 			std::vector<node*> path = { this };
-			for (node* ndptr = this; ndptr->is_selectable(); path.push_back(ndptr)) {
+			while(ndptr->is_selectable()){
 				ndptr = &*std::max_element(ndptr->child.begin(), ndptr->child.end(),
-						[=](const node& lhs, const node& rhs) { return lhs.ucb_score() < rhs.ucb_score(); });
+						[=](const node& lhs, const node& rhs) { return lhs.ucb_score(rave_win, rave_visit) < 
+																	   rhs.ucb_score(rave_win, rave_visit); });
+				path.push_back(ndptr)
 			}
 			return path;
 		}
@@ -157,13 +163,14 @@ public:
 		 * expand the current node and return the newly expanded child node
 		 * if the current node has no unexpanded move, it returns itself
 		 */
+
 		node* expand(std::default_random_engine& engine) {
 			board child_state = *this;
 			std::vector<int> moves = all_moves(engine);
 			auto expanded_move = std::find_if(moves.begin(), moves.end(), [&](int move) {
 				// check whether it is an unexpanded legal move
 				bool is_expanded = std::find_if(child.begin(), child.end(),
-						[&](const node& node) { return node.info().last_move_index == move; }) != child.end();
+						[&](const node& node) { return node.info().last_move_index == move; }) != child.end(); //last_move_index is the 1-d position index of last move
 				return is_expanded == false && child_state.place(move) == board::legal;
 			});
 			if (expanded_move == moves.end()) return this; // already terminal
@@ -185,11 +192,28 @@ public:
 		/**
 		 * update statistics for all nodes saved in the path
 		 */
-		void update(std::vector<node*>& path, unsigned winner) {
+		void update(std::vector<node*>& path, unsigned winner, 
+			std::unordered_map<board::piece_type, std::unordered_map<int, int>> &rave_win,
+			std::unordered_map<board::piece_type, std::unordered_map<int, int>> &rave_visit) {
 			for (node* ndptr : path) {
 				ndptr->win += (winner == info().who_take_turns) ? 1 : 0;
 				ndptr->visit += 1;
+				update_rave(ndptr, winner, rave_win, rave_visit);
 			}
+		}
+
+		void update_rave(node* ndptr, int winner,
+				std::unordered_map<board::piece_type, std::unordered_map<int, int>> &rave_win,
+				std::unordered_map<board::piece_type, std::unordered_map<int, int>> &rave_visit) {
+			// update rave value
+			board::piece_type color = ndptr.info().who_take_turns;
+			int move = ndptr.info().last_move_index;
+			if (rave_win.count(who) == 0) rave_win[color] = std::unordered_map<size_t, float>();
+			if (rave_win[color].count(move) == 0) rave_win[color][move] = 0;
+			if (winner == color) {
+				rave_win[color][move]++;
+			}
+			rave_visit[color][move]++;
 		}
 
 		/**
@@ -209,19 +233,28 @@ public:
 		 */
 		bool is_selectable() const {
 			size_t num_moves = 0;
-			for (int move = 0; move < 81; move++)
+			int move = 0;
+			while(move < 81){
 				if (board(*this).place(move) == board::legal)
 					num_moves++;
+				move++;
+			}
 			return child.size() == num_moves && num_moves > 0;
 		}
 
 		/**
 		 * get the ucb score of this node
 		 */
-		float ucb_score(float c = std::sqrt(2)) const {
-			float exploit = float(win) / visit;
-			float explore = std::sqrt(std::log(parent->visit) / visit);
-			return exploit + c * explore;
+		float ucb_score(float c = std::sqrt(2),
+			std::unordered_map<board::piece_type, std::unordered_map<int, int>> rave_win,
+			std::unordered_map<board::piece_type, std::unordered_map<int, int>> rave_visit) const {
+			board::piece_type color = info().who_take_turns;
+			int move = info().last_move_index;
+			float numerator = float(rave_win[color][move]) + float(win) + std::sqrt(log_visits_ * child.visits_) * c;
+			float denominator = visit + rave_visit[color][move];
+			//float exploit = float(win) / visit;
+			//float explore = std::sqrt(std::log(parent->visit) / visit);
+			return numerator / denominator;
 		}
 
 		/**
@@ -229,14 +262,20 @@ public:
 		 */
 		std::vector<int> all_moves(std::default_random_engine& engine) const {
 			std::vector<int> moves;
-			for (int move = 0; move < 81; move++) moves.push_back(move);
+			int move = 0;
+			while(move < 81){
+				moves.push_back(move);
+				move++;
+			}	
 			std::shuffle(moves.begin(), moves.end(), engine);
 			return moves;
 		}
 
 	private:
-		size_t win, visit;
+		size_t win, visit, rave_win, rave_visit;
 		std::vector<node> child;
 		node* parent;
 	};
+private:
+
 };
